59997ed204f00e33fcd5ab3dbf5c582a3d25eea2
B.J.J. Lochan <brian.lochan@hva.nl>
2019-09-17 16:06:57 +0200

    Frontend changes to app.py

--- WeatherFlask/app.py
+++ WeatherFlask/app.py
@@ -1,9 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask
+import json
+import requests
+import time
+import atexit
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
 app = Flask(__name__)
+
+
+def request_and_save_weather_data():
+
+    params = (
+        ('locatie', 'Amsterdam'),
+        ('key', '85c9b987c6'),
+    )
+
+    response = requests.get('https://meteoserver.nl/api/uurverwachting.php', params=params)
+    content = response.content
+
+    file = open("weather.txt", "w+")
+    file.write(str(response.content)[2:-1])
+    print('wrote new weather data file')
+
+
+# initiate scheduler for retrieving weather data
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=request_and_save_weather_data, trigger="interval", hours=1)
+scheduler.start()
+
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 @app.route('/<class_group>')
 def class_group(class_group):
    return render_template('/MyTimetable.htm', class_group = class_group)
 
+@app.route('/city')
+def show_city():
+    with open("weather.txt") as f:
+        data = json.load(f)
+
+    return data['plaatsnaam'][0]["plaats"]
+
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run()
+
