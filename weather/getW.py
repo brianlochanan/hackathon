8b6c2ea34db9eb22251eb496d02882dba507c043
Ankris <ventura.carriazo@gmail.com>
2019-09-18 14:42:10 +0200

    json for weather

--- weather/getW.py
+++ weather/getW.py
@@ -3,34 +3,59 @@ import datetime
 
 with open("weather.txt") as f:
     data = json.load(f)
-    print(data)
+    # print(data)
 
-print(data['plaatsnaam'][0]["plaats"])
+# print(data['plaatsnaam'][0]["plaats"])
 
-time = datetime.datetime.now()
+# time = datetime.datetime.now()
 time2 = data["data"][0]["tijd_nl"]
 timeW = datetime.datetime.strptime(time2, "%d-%m-%Y %H:%M")
-print(time)
-print(time2)
-print(timeW)
+
+# print(time)
+# print(time2)
+# print(timeW)
 
 
 def preweather(start):
     dif = start - timeW.hour
-    s1 = []
+    s1 = {}
+    Temperature = []
+    Date = []
+    WindSpeed = []
+    Precipitations = []
     for i in range(dif):
-        s1.append([data["data"][i]["tijd_nl"], data["data"][i]["temp"],
-                   data["data"][i]["windkmh"], data["data"][i]["neersl"]])
+        Temperature.append(data["data"][i]["temp"]), Date.append(data["data"][i]["tijd_nl"])
+        # To transform the date into daytime form use datetime.datetime.strptime(Date[x], "%d-%m-%Y %H:%M")
+        WindSpeed.append(data["data"][i]["windkmh"]), Precipitations.append(data["data"][i]["neersl"])
 
-    return s1
+    s1.update({"Date": Date})
+    s1.update({"Temperature": Temperature})
+    s1.update({"Wind Speed": WindSpeed})
+    s1.update({"Precipitations": Precipitations})
+    y = json.dumps(s1)
+    return y
 
 
 def postweather(end):
     dif2 = end - timeW.hour
-    s2 = []
-    for j in range(dif2 + 1, dif2 + 3):
-        s2.append([data["data"][j]["tijd_nl"], data["data"][j]["temp"],
-                   data["data"][j]["windkmh"], data["data"][j]["neersl"]])
+    s2 = {}
+    Temperature = []
+    Date = []
+    WindSpeed = []
+    Precipitations = []
+    for j in range(dif2, dif2 + 2):
+        Temperature.append(data["data"][j]["temp"]), Date.append(data["data"][j]["tijd_nl"])
+        # To transform the date into daytime form use datetime.datetime.strptime(Date[x], "%d-%m-%Y %H:%M")
+        WindSpeed.append(data["data"][j]["windkmh"]), Precipitations.append(data["data"][j]["neersl"])
 
-    return s2
+    s2.update({"Date": Date})
+    s2.update({"Temperature": Temperature})
+    s2.update({"Wind Speed": WindSpeed})
+    s2.update({"Precipitations": Precipitations})
+    y = json.dumps(s2)
 
+    return y
+
+
+# print(preweather(16))
+# print(postweather(20))
