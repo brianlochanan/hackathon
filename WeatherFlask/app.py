6565a2f72bc020f14361c43c9d70dd9fc49ee11a
B.J.J. Lochan <brian.lochan@hva.nl>
2019-09-19 17:44:38 +0200

    Made input field to search class

--- WeatherFlask/app.py
+++ WeatherFlask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, make_response
+from flask import Flask, request, render_template, redirect, make_response
 from datetime import datetime
 import json
 import requests
@@ -15,7 +15,6 @@ app = Flask(__name__)
 
 
 def request_and_save_weather_data():
-
     params = (
         ('locatie', 'Amsterdam'),
         ('key', '85c9b987c6'),
@@ -37,26 +36,36 @@ scheduler.start()
 # Shut down the scheduler when exiting the app
 atexit.register(lambda: scheduler.shutdown())
 
+
 # this route should return the home page. i.e. the recommendation on what to wear
-@app.route('/', methods=['GET'])
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def selectCourse():
+    # x = input('Add the first leters of your course')
+    # return redirect('/class/' + x)
+    if request.method == 'POST':
+        course = request.form['Course']
+        print(course)
+        return redirect('/class/' + course)
+
+    # Give HTML data to work with
+    return render_template('SelectCourse.html')
+
 
 @app.route('/class/<class_group>', methods=['GET', 'POST'])
 def test(class_group):
-
     scheduleApi = ScheduleApi(class_group)
     getW = GetWeather()
     schedule_result = (scheduleApi.get_schedule_for_class_group())
 
     # Give HTML data to work with
     return render_template('MyTimetable.htm',
-                           class_group = class_group,
-                           result = schedule_result,
-                           length = len(schedule_result),
-                           weather = getW.weather10(),
-                           weather_length = len(getW.weather10()['Date'])
+                           class_group=class_group,
+                           result=schedule_result,
+                           length=len(schedule_result),
+                           weather=getW.weather10(),
+                           weather_length=len(getW.weather10()['Date'])
                            )
+
 
 # not sure if we need this
 @app.route('/city')
@@ -65,8 +74,9 @@ def show_city():
         data = json.load(f)
 
     return data['plaatsnaam'][0]["plaats"]
-    
-@app.route('/addCourse', methods=['GET','POST'])
+
+
+@app.route('/addCourse', methods=['GET', 'POST'])
 def add_course():
     if request.method == 'GET':
         """show interface for adding courses"""
@@ -78,4 +88,3 @@ def add_course():
 
 if __name__ == '__main__':
     app.run()
-
---
+++ WeatherFlask/static/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/visualcookie/pen/kkwxPm.
+
+ Inspired by Daniel Luca. https://dribbble.com/shots/2989456-Email-input-field
